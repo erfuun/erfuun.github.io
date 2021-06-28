8931d0e04aee2af4b9c124500c53a3e5eb22cbe2
Erfansadeghinezhad <erfansane5150@gmail.com>
2021-06-28 11:30:30 +0430

    add header to root

--- src/App.vue
+++ src/App.vue
@@ -1,11 +1,20 @@
 <template>
-  <div id="app"></div>
+  <div id="app">
+    <div class="page-content">
+      <div id="content">
+        <app-header></app-header>
+      </div>
+    </div>
+  </div>
 </template>
 
 <script lang="ts">
 import { defineComponent } from "vue";
-
+import AppHeader from "./components/Header.vue";
 export default defineComponent({
   setup() {},
+  components: {
+    AppHeader,
+  },
 });
 </script>
