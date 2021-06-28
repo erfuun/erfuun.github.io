0035c5256b799d88b8d7265313642423396ea513
Erfansadeghinezhad <erfansane5150@gmail.com>
2021-06-28 13:45:20 +0430

    convert css file to tailwind utility

--- tailwind.config.js
+++ tailwind.config.js
@@ -1,11 +1,25 @@
 module.exports = {
-  purge: ['./index.html', './src/**/*.{vue,js,ts,jsx,tsx}'],
+  purge: ["./index.html", "./src/**/*.{vue,js,ts,jsx,tsx}"],
   darkMode: false, // or 'media' or 'class'
   theme: {
-    extend: {},
+    extend: {
+      lineHeight: {
+        12: "3rem"
+      }
+    }
   },
   variants: {
-    extend: {},
+    extend: {}
   },
-  plugins: [],
-}
+  plugins: [
+    require("tailwindcss-pseudo-elements")({
+      customPseudoClasses: ["foo"],
+      customPseudoElements: ["bar"],
+      contentUtilities: false,
+      emptyContent: false,
+      classNameReplacer: {
+        "hover:before:text-black": "hbt"
+      }
+    })
+  ]
+};
