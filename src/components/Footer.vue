72d30b04771b500d7b8a97a026233b2c181da780
Erfansadeghinezhad <erfansane5150@gmail.com>
2021-07-02 17:39:34 +0430

    added footer

---
+++ src/components/Footer.vue
@@ -0,0 +1,52 @@
+<template>
+  <footer class="pt-4 pb-4 text-center bg-light">
+    <div class="container">
+      <div class="my-3">
+        <div class="h4">Erfan Sadeghinezhad</div>
+        <p>Full Stack Web Developer</p>
+        <div class="social-nav">
+          <nav role="navigation">
+            <ul class="nav justify-content-center">
+              <li class="nav-item">
+                <a
+                  class="nav-link"
+                  href="https://twitter.com/@nezhad.erfan"
+                  title="Twitter"
+                  ><i class="fab fa-twitter"></i
+                  ><span class="menu-title sr-only">Twitter</span></a
+                >
+              </li>
+              <li class="nav-item">
+                <a
+                  class="nav-link"
+                  href="https://github.com/efnninja"
+                  title="Github"
+                  ><i class="fab fa-github"></i
+                  ><span class="menu-title sr-only">Github</span></a
+                >
+              </li>
+              <li class="nav-item">
+                <a
+                  class="nav-link"
+                  href="https://www.instagram.com/erfan99sadeqi"
+                  title="Instagram"
+                  ><i class="fab fa-instagram"></i
+                  ><span class="menu-title sr-only">Instagram</span></a
+                >
+              </li>
+              <li class="nav-item">
+                <a
+                  class="nav-link"
+                  href="https://www.linkedin.com/"
+                  title="LinkedIn"
+                  ><i class="fab fa-linkedin"></i
+                  ><span class="menu-title sr-only">LinkedIn</span></a
+                >
+              </li>
+            </ul>
+          </nav>
+        </div>
+      </div>
+    </div>
+  </footer>
+</template>
